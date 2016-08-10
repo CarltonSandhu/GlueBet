f4351a00fa2b6cba1f2e5efac0f628bd1adff9cf
CarltonSandhu <carlton.sandhu@gmail.com>
2016-08-10 21:55:05 +0100

    Minor Tinkers

--- app/src/main/java/com/example/carlt/gluebet/LoggedOutActivity.java
+++ app/src/main/java/com/example/carlt/gluebet/LoggedOutActivity.java
@@ -2,6 +2,10 @@ package com.example.carlt.gluebet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoggedOutActivity extends AppCompatActivity {
 
@@ -10,4 +14,12 @@ public class LoggedOutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_out);
     }
+
+    public void errorcheck(View view){
+        EditText editText = (EditText) findViewById(R.id.EmailEdit);
+        if (editText.getText().toString().equals("")){
+            Toast.makeText(this,"FUCKING WRONG NIGGA",Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
